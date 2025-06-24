220bc007016d1d6ffbaf3fa0ee4083b24b0df663
Alexander Bost <alexander.c.bost@gmail.com>
2025-06-24 18:10:24 -0400

    Refactored backprop.

--- neuralnet_class.py
+++ neuralnet_class.py
@@ -22,7 +22,7 @@ class neural_net:
             n_in = self.layers[l-1]
             n_out = self.layers[l]
 
-            params[weight_key] = np.random.randn(n_in,n_out) * np.sqrt(2.0 / (n_in))
+            params[weight_key] = np.random.randn(n_in,n_out) * np.sqrt(1.0 / (n_in))
             params[bias_key] = np.zeros((1,n_out))
 
         return params
@@ -37,7 +37,9 @@ class neural_net:
 
 
     def _sigmoid(self,x):
-        return 1 / (1 + np.exp(-(x)))
+        # Clip x to prevent overflow
+        x_clipped = np.clip(x, -500, 500)
+        return 1 / (1 + np.exp(-x_clipped))
 
 
     def _d_sigmoid(self,x):
@@ -71,8 +73,10 @@ class neural_net:
         return A
 
 
-    def _backprop(self, y_hat, y_true):
-        L = self._calcloss(y_hat,y_true)
+ def _backprop(self, y_hat, y_true):
+        m = y_hat.shape[0]  # batch size
+        L = self._calcloss(y_hat, y_true)
+        
         gradients = {
             'dLdZ': {},
             'dLdA': {},
@@ -80,22 +84,25 @@ class neural_net:
             'dLdb': {}
         }
 
-        for l in range(self.num_layers-1,0,-1):
-            if l == self.num_layers - 1 and 'BCE' in self.cache:
-                gradients['dLdA'][l] = (y_hat - y_true)
-                gradients['dLdZ'][l] = (y_hat - y_true) * self._d_sigmoid(self.cache[f'Z{l}'])
-                gradients['dLdA'][l-1] = gradients['dLdZ'][l] @ self.parameters[f'W{l}'].T  
-            elif l < self.num_layers - 1 and self.cache[f'activation{l}'] == 'relu' and 'BCE' in self.cache:
-                gradients['dLdA'][l] = gradients['dLdZ'][l+1] @ self.parameters[f'W{l+1}'].T
-                gradients['dLdZ'][l] = gradients['dLdA'][l] * self._d_relu(self.cache[f'Z{l}'])
+        # Start from output layer and work backwards
+        for l in range(self.num_layers-1, 0, -1):
+            if l == self.num_layers - 1:
+                # Output layer - for sigmoid + BCE loss
+                gradients['dLdZ'][l] = (y_hat - y_true) / m
             else:
-                # If this runs, either the loss type was changed from BCE to something else without accounting for it,
-                # or _calcloss() wasn't called somehow.
-                print("Only code for binary cross entropy has been written, but self.cache doesn't have stored BCE values. Exiting program.")
-                sys.exit()
+                # Hidden layers
+                if self.cache[f'activation{l}'] == 'relu':
+                    gradients['dLdZ'][l] = gradients['dLdA'][l] * self._d_relu(self.cache[f'Z{l}'])
+                else:
+                    gradients['dLdZ'][l] = gradients['dLdA'][l] * self._d_sigmoid(self.cache[f'Z{l}'])
 
+            # Compute weight and bias gradients
             gradients['dLdW'][l] = self.cache[f'A{l-1}'].T @ gradients['dLdZ'][l]
-            gradients['dLdb'][l] = np.sum(gradients['dLdZ'][l], axis=0)
+            gradients['dLdb'][l] = np.sum(gradients['dLdZ'][l], axis=0, keepdims=True)
+            
+            # Compute gradient w.r.t. previous layer's activations (except for input layer)
+            if l > 1:
+                gradients['dLdA'][l-1] = gradients['dLdZ'][l] @ self.parameters[f'W{l}'].T
 
         return gradients
 
@@ -107,37 +114,49 @@ class neural_net:
 
 
     def _calcloss(self, y_hat, y_true):
-        self.cache['BCE'] = np.mean(-(y_true * np.log(y_hat) + (1 - y_true) * np.log(1-y_hat)))
+        epsilon = 1e-15
+        y_hat_clipped = np.clip(y_hat, epsilon, 1 - epsilon)
+
+        self.cache['BCE'] = -np.mean(y_true * np.log(y_hat_clipped) + (1 - y_true) * np.log(1 - y_hat_clipped))
         # self.cache['MSE'] = np.mean(0.5 * (y_hat - y_true) ** 2)
         return self.cache['BCE']
     
 
     def train_network(self, X, y, epochs=500, batch_size=32):
-        num_samples = len(X[0])
+        num_samples = len(X)
+        print(f"Num_samples: {num_samples}\n")
         num_batches = (num_samples + batch_size - 1) // batch_size
 
         leftover_datapoints = num_samples % batch_size
 
-        total_epoch_loss = 0
-    
-        shuffled_indices = np.random.permutation(num_samples)
-        shuffled_X = X[shuffled_indices]
-        shuffled_y = y[shuffled_indices]
+        for epoch in range(epochs):
+            total_epoch_loss = 0
+        
+            shuffled_indices = np.random.permutation(num_samples)
+            shuffled_X = X[shuffled_indices]
+            shuffled_y = y[shuffled_indices]
 
-        for i in range(num_batches):
-            # i represents each batch
-            start_idx = i * batch_size
-            end_idx = min((i + 1) * batch_size, num_samples) # Ensure we don't go out of bounds for the last batch
+            for i in range(num_batches):
+                # i represents each batch
+                start_idx = i * batch_size
+                end_idx = min((i + 1) * batch_size, num_samples) # Ensure we don't go out of bounds for the last batch
+                
+                current_batch_X = shuffled_X[start_idx:end_idx]
+                current_batch_y = shuffled_y[start_idx:end_idx]
+                current_batch_size = len(current_batch_X)
+
+                y_hat = self._forward_pass(current_batch_X)
+
+                batch_grad = self._backprop(y_hat, current_batch_y)
+
+                self._update_params(batch_grad)
+
+                batch_loss = self._calcloss(y_hat, current_batch_y)
+                total_epoch_loss += batch_loss
             
-            current_batch_X = shuffled_X[start_idx:end_idx]
-            current_batch_y = shuffled_y[start_idx:end_idx]
-            current_batch_size = len(current_batch_X)
-
-            y_hat = self._forward_pass(current_batch_X)
-
-            batch_grad = self._backprop(y_hat, current_batch_y)
-
-            self._update_params(batch_grad)
+            if epoch % 50 == 0:
+                avg_loss = total_epoch_loss / num_batches
+                print(f"Epoch {epoch}, Average Loss: {avg_loss:.6f}")
 
                 
 net1 = neural_net([2,10,10,10,1])
